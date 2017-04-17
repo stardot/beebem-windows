1bb256642755ecb998a13f9cdbbb95ff55453b56
Chris Needham <chris@chrisneedham.com>
2017-04-17 13:02:05 +0100

    Added #include for memset()

--- Src/ARMulator/arminit.c
+++ Src/ARMulator/arminit.c
@@ -19,6 +19,8 @@ Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. 
 #include "armemu.h"
 #include "dbg_rdi.h"
 
+#include <string.h>
+
 /***************************************************************************\
 *                 Definitions for the emulator architecture                 *
 \***************************************************************************/
