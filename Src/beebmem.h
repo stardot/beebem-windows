69d88b92fb2ed71a6c03267b3ea51775eb14ecb9
dominicbeesley <dominic@authorityfile.co.uk>
2019-11-26 12:41:24 +0000

    m5000 device selection register fixes
    Fix more correctly emulate the behaviour of the &FCFF register - only return the contents when the device has been selected.

--- Src/beebmem.h
+++ Src/beebmem.h
@@ -130,6 +130,6 @@ void LoadFileMemUEF(FILE *SUEF);
 void LoadSWRamMemUEF(FILE *SUEF);
 void LoadSWRomMemUEF(FILE *SUEF);
 void LoadIntegraBHiddenMemUEF(FILE *SUEF);
-void LoadJIMPageRegUEF(FILE *SUEF);
+//void LoadJIMPageRegUEF(FILE *SUEF);
 void DebugMemoryState();
 #endif
