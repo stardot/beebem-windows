7af9d128e4f25d828fe88c5c3519ab713ef24111
Chris Needham <chris@chrisneedham.com>
2020-12-28 22:35:44 +0000

    Refactored csw.cpp
    - Use enum for states
    - Remove unused extern declarations
    - Fix indentation
    - Declare variables at first use

--- Src/csw.h
+++ Src/csw.h
@@ -41,21 +41,20 @@ int csw_data(void);
 int csw_poll(int clock);
 void map_csw_file(void);
 
-extern FILE *csw_file;
-extern unsigned char file_buf[BUFFER_LEN];
-extern unsigned char *csw_buff;
-extern unsigned char *sourcebuff;
+enum class CSWState {
+	WaitingForTone,
+	Tone,
+	Data,
+	Undefined
+};
 
-extern int csw_tonecount;
-extern int csw_state;
-extern int csw_datastate;
+extern CSWState csw_state;
+
 extern int csw_bit;
 extern int csw_pulselen;
 extern int csw_ptr;
 extern unsigned long csw_bufflen;
-extern int csw_byte;
 extern int csw_pulsecount;
-extern int bit_count;
 extern bool CSWOpen;
 extern int CSW_BUF;
 extern int CSW_CYCLES;
