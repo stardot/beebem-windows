dc2da72e5fcc330368f5ed538dc9b706b3a59c22
Chris Needham <chris@chrisneedham.com>
2024-12-23 15:46:33 +0000

    Fixed Master 512 80186 co-pro timing table

--- Src/Master512CoPro.cpp
+++ Src/Master512CoPro.cpp
@@ -164,64 +164,74 @@ enum
 
 static const uint8_t Timing[] =
 {
-	51,32,              // exception, IRET
-	2, 0, 4, 2,         // INTs
-	2,                  // segment overrides
-	2, 4, 4,            // flag operations
-	4, 4,83,60,         // arithmetic adjusts
-	4, 4,               // decimal adjusts
-	2, 5,               // sign extension
-	2,24, 2, 2, 3,11,   // misc
+	45, 28,           // exception, IRET
+	0, 2, 4, 3,       // INTs
+	2,                // segment overrides
+	2, 2, 3,          // flag operations
+	8, 7, 19, 15,     // arithmetic adjusts
+	4, 4,             // decimal adjusts
+	2, 4,             // sign extension
+	2,18, 6, 2, 6,11, // misc
 
-	15,15,15,           // direct JMPs
-	11,18,24,           // indirect JMPs
-	19,28,              // direct CALLs
-	16,21,37,           // indirect CALLs
-	20,32,24,31,        // returns
-	4,16, 6,18,         // conditional JMPs
-	5,17, 6,18,         // loops
+	14,14,14,         // direct JMPs
+	11,17,26,         // indirect JMPs
+	15,23,            // direct CALLs
+	13,19,38,         // indirect CALLs
+	16,22,18,25,      // returns
+	4,13, 5,15,       // conditional JMPs
+	6,16, 6,16,       // loops
 
-	10,14, 8,12,        // port reads
-	10,14, 8,12,        // port writes
+	10,10, 8, 8,      // port reads
+	9, 9, 7, 7,       // port writes
 
-	2, 8, 9,            // move, 8-bit
-	4,10,               // move, 8-bit immediate
-	2, 8, 9,            // move, 16-bit
-	4,10,               // move, 16-bit immediate
-	10,10,10,10,        // move, AL/AX memory
-	2, 8, 2, 9,         // move, segment registers
-	4,17,               // exchange, 8-bit
-	4,17, 3,            // exchange, 16-bit
+	2, 9,12,          // move, 8-bit
+	3,12,             // move, 8-bit immediate
+	2, 9,12,          // move, 16-bit
+	4,13,             // move, 16-bit immediate
+	8, 8, 9, 9,       // move, AL/AX memory
+	2,11, 2,11,       // move, segment registers
+	4,17,             // exchange, 8-bit
+	4,17, 3,          // exchange, 16-bit
 
-	15,24,14,14,        // pushes
-	12,25,12,12,        // pops
+	10,16, 9, 9,      // pushes
+	10,20, 8, 8,      // pops
 
-	3, 9,16,            // ALU ops, 8-bit
-	4,17,10,            // ALU ops, 8-bit immediate
-	3, 9,16,            // ALU ops, 16-bit
-	4,17,10,            // ALU ops, 16-bit immediate
-	4,17,10,            // ALU ops, 16-bit w/8-bit immediate
-	70,118,76,128,      // MUL
-	80,128,86,138,      // IMUL
-	80,144,86,154,      // DIV
-	101,165,107,175,    // IDIV
-	3, 2,15,15,         // INC/DEC
-	3, 3,16,16,         // NEG/NOT
+	3,10,10,          // ALU ops, 8-bit
+	4,16,10,          // ALU ops, 8-bit immediate
+	3,10,10,          // ALU ops, 16-bit
+	4,16,10,          // ALU ops, 16-bit immediate
+	4,16,10,          // ALU ops, 16-bit w/8-bit immediate
+	26,35,32,41,      // MUL
+	25,34,31,40,      // IMUL
+	29,38,35,44,      // DIV
+	44,53,50,59,      // IDIV
+	3, 3,15,15,       // INC/DEC
+	3, 3,10,10,       // NEG/NOT
 
-	2, 8, 4,            // reg shift/rotate
-	15,20, 4,           // m8 shift/rotate
-	15,20, 4,           // m16 shift/rotate
+	2, 5, 1,          // reg shift/rotate
+	15,17, 1,         // m8 shift/rotate
+	15,17, 1,         // m16 shift/rotate
 
-	22, 9,21,           // CMPS 8-bit
-	22, 9,21,           // CMPS 16-bit
-	15, 9,14,           // SCAS 8-bit
-	15, 9,14,           // SCAS 16-bit
-	12, 9,11,           // LODS 8-bit
-	12, 9,11,           // LODS 16-bit
-	11, 9,10,           // STOS 8-bit
-	11, 9,10,           // STOS 16-bit
-	18, 9,17,           // MOVS 8-bit
-	18, 9,17,           // MOVS 16-bit
+	22, 5,22,         // CMPS 8-bit
+	22, 5,22,         // CMPS 16-bit
+	15, 5,15,         // SCAS 8-bit
+	15, 5,15,         // SCAS 16-bit
+	12, 6,11,         // LODS 8-bit
+	12, 6,11,         // LODS 16-bit
+	10, 6, 9,         // STOS 8-bit
+	10, 6, 9,         // STOS 16-bit
+	14, 8, 8,         // MOVS 8-bit
+	14, 8, 8,         // MOVS 16-bit
+
+	14, 8, 8,         // (80186) INS 8-bit
+	14, 8, 8,         // (80186) INS 16-bit
+	14, 8, 8,         // (80186) OUTS 8-bit
+	14, 8, 8,         // (80186) OUTS 16-bit
+	14,68,83,         // (80186) PUSH immediate, PUSHA/POPA
+	22,29,            // (80186) IMUL immediate 8-bit
+	25,32,            // (80186) IMUL immediate 16-bit
+	15,25,4,16, 8,    // (80186) ENTER/LEAVE
+	33,               // (80186) BOUND
 };
 
 // DMA control register
